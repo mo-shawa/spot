e7856ec7cf5e62419849982829300a42c2b5b54f
Mo Shawa <moshawa0@gmail.com>
2021-12-07 02:16:54 -0500

    basic file structure setup

--- spot/settings.py
+++ spot/settings.py
@@ -75,8 +75,8 @@ WSGI_APPLICATION = 'spot.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'spot',
     }
 }
 
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'EST'
 
 USE_I18N = True
 
