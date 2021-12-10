e556818bbfec146d8110ff7b7c591fe722bf61b1
Mo Shawa <moshawa0@gmail.com>
2021-12-09 22:15:12 -0500

    add and configure crispy, login and signup basic styling

--- spot/settings.py
+++ spot/settings.py
@@ -38,7 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap'
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
