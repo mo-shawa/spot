d0f8c59bbc772614c790d207b9d52a0ca2aeaaee
Matt Lobo <mattl99@hotmail.ca>
2021-12-12 10:50:43 -0500

    profile and user connection working via admin but not through site forms

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,7 +1,12 @@
 from django.forms import ModelForm
-from .models import User, Dog
+from .models import User, Dog, Profile
 
 class UserForm(ModelForm):
     class Meta:
         model = User
         fields = ['username', 'first_name', 'last_name', 'email', 'password' ]
+
+class ProfileForm(ModelForm):
+    class Meta:
+        model = Profile
+        fields = "__all__"
--- main_app/models.py
+++ main_app/models.py
@@ -3,6 +3,8 @@ from django.urls import reverse
 from datetime import date
 # Import the User
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 # Create your models here.
 
 class Profile(models.Model):
@@ -11,7 +13,14 @@ class Profile(models.Model):
     bio = models.TextField(max_length=250)
     # friends = models.ManyToManyField(Profile)
 
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
 
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
 
 class Dog(models.Model): 
     name = models.CharField(max_length=100)
