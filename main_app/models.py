f040e7cb5f3d3ac9c0faff8ff66e00f74e1aec7f
Matt Lobo <mattl99@hotmail.ca>
2021-12-13 15:43:57 -0500

    testing ssh - ignore this

--- main_app/models.py
+++ main_app/models.py
@@ -23,7 +23,7 @@ class Profile(models.Model):
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
-
+# hello
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
