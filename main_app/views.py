26dc7572678d7476b0231da8156e5910ba953fee
Matt Lobo <mattl99@hotmail.ca>
2021-12-13 16:32:57 -0500

    almost done adding photos

--- main_app/views.py
+++ main_app/views.py
@@ -6,8 +6,14 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from main_app.forms import ProfileForm
-from main_app.models import Dog, Profile
-# Create your views here.
+import boto3
+import uuid
+from main_app.models import Dog, Profile, Photo
+
+
+S3_BASE_URL = 'https://s3.us-east-2.amazonaws.com/'
+BUCKET = 'hellofren'
+
 
 def home(request):
   return render(request, 'home.html')
@@ -73,3 +79,21 @@ class DogCreate(LoginRequiredMixin,CreateView):
   def form_valid(self,form):
     form.instance.user.profile = self.request.user.profile
     return super().form_valid(form)
+
+def profile_photo(request, user_id):
+  photo_file = request.FILES.get("photo-file", None)
+  if photo_file:
+        s3 = boto3.client('s3')
+        # need a unique "key" for S3 / needs image file extension too
+        key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
+        # just in case something goes wrong
+        try:
+            s3.upload_fileobj(photo_file, BUCKET, key)
+            # build the full url string
+            url = f"{S3_BASE_URL}{BUCKET}/{key}"
+            # we can assign to cat_id or cat (if you have a cat object)
+            photo = Photo(url=url, user_id=user_id)
+            photo.save()
+        except:
+            print('An error occurred uploading file to S3')
+  return redirect('profile_detail', user_id=user_id)
