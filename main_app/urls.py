ed6c31378e3ff76ff1fcd0bff9a381389655b252
Mo Shawa <moshawa0@gmail.com>
2021-12-13 16:31:26 -0500

    fixed signup redirect

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,8 +6,9 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('accounts/signup/', views.signup, name='signup'),
     path('accounts/signup/<int:user_id>/', views.ProfileCreate.as_view(), name="profile_create"),
+    
     path('accounts/<int:user_id>', views.profile_detail, name='profile_detail'),
     # DOG PATHS
     path('dogs/create/',views.DogCreate.as_view(), name='dog_create'),
-    path('accounts/<int:user_id>/update', views.profile_update, name='profile_update')
+    path('accounts/update', views.profile_update, name='profile_update')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from main_app.forms import ProfileForm
 from main_app.models import Dog, Profile
+
 # Create your views here.
 
 def home(request):
@@ -15,33 +16,23 @@ def home(request):
 def signup(request):
   error_message = ''
   if request.method == 'POST':
-    # This is how to create a 'user' form object
-    # that includes the data from the browser
     user_form = UserCreationForm(request.POST)
-    profile_form = ProfileForm(request.POST, instance=request.user.profile)    
-    if user_form.is_valid() and profile_form.is_valid():
+    if user_form.is_valid():
       user = user_form.save()
-      profile_form.save()
       login(request, user)
-      
-      return redirect('')
-      
-
-      return redirect('profile_create', user_id=user_id )
+      return redirect('profile_update')
     else:
       error_message = 'Invalid sign up - try again'
   # A bad POST or a GET request, so render signup.html with an empty form
   form = UserCreationForm()
-  profile_form = ProfileForm()    
-
-  context = {'user_form': form, 'profile_form': profile_form, 'error_message': error_message}
+  context = {'user_form': form, 'error_message': error_message}
   return render(request, 'registration/signup.html', context)
 
 class ProfileCreate(LoginRequiredMixin, CreateView):
   model = Profile
   fields = "__all__"
 
-def profile_update(request, user_id):
+def profile_update(request):
   profile_form = ProfileForm() 
   context= {"profile_form": profile_form}
   return render(request, 'main_app/profile_form.html', context)
