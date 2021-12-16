8bd33edd9635bc3a3e25c0c47a0632fc1cd7d8f7
Matt Lobo <mattl99@hotmail.ca>
2021-12-16 10:59:49 -0500

    dog profile photos working

--- main_app/views.py
+++ main_app/views.py
@@ -105,12 +105,12 @@ class DogCreate(LoginRequiredMixin,CreateView):
   def form_valid(self,form):
     profile = Profile.objects.get(user = self.request.user.id)
     form.instance.profile = profile
-    print(self.request.POST)
+    print("self.request.POST",self.request.POST)
     photo_file = self.request.FILES.get("photo-file", None)
     print(photo_file)
     if photo_file:
     
-        print("photofile")
+       
         s3 = boto3.client('s3')
         key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
        
@@ -118,13 +118,14 @@ class DogCreate(LoginRequiredMixin,CreateView):
             s3.upload_fileobj(photo_file, BUCKET, key)
             # build the full url string
             url = f"{S3_BASE_URL}{BUCKET}/{key}"
-            
+            form.instance.image = url
             print("hello",self.request.POST)
-        except:
-            print('An error occurred uploading file to S3')
+        except Exception as e:
+
+            print('An error occurred uploading file to S3: ',e)
         
-        form.instance.image = url
-        self.request.POST.image = url
+        
+        
         super().form_valid(form)
     return redirect('profile_detail')
     
