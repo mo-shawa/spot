e515c8bf689a894295b72c68d8f6ff1185931161
Mo Shawa <moshawa0@gmail.com>
2021-12-16 19:02:55 -0500

    form alignment and styling

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.forms import ModelForm
+from django.forms import ModelForm, widgets
 from .models import Comment, User, Dog, Profile
 
 class UserForm(UserCreationForm):
@@ -11,6 +11,7 @@ class UserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'first_name', 'last_name', 'email', 'password1','password2' ]
+        
 
 class ProfileForm(ModelForm):
     class Meta:
